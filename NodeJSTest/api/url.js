01945cb06d1a3cab43e19e1e4eb6127c673bdca5
chuyang2 <chuyang3@jd.com>
2018-08-24 17:52:45 +0800

    API示例

---
+++ NodeJSTest/api/url.js
@@ -0,0 +1,9 @@
+// 所有http请求，路径请求都要用到url
+// 解析出url对象
+url.parse('http://baidu.com')
+
+// 解析处的query是一个对象，默认的是一个字符串
+url.parse('http://baidu.com', true)
+
+// 不确定是http还是https的情况下这样解析
+url.parse('//baidu.com')

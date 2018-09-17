f0e51fd4c206e5529a68d1035ddb6d7c01601119
chuyang2 <chuyang3@jd.com>
2018-09-17 22:20:34 +0800

    NodeJS 学习示例

---
+++ JDKNewFeature/src/main/java/com/tristan/viii/testcodes/TestArrayList.java
@@ -0,0 +1,21 @@
+package com.tristan.viii.testcodes;
+
+import java.util.*;
+
+public class TestArrayList {
+
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+
+        System.out.println(list.get(0));
+        list.remove(0);
+
+        List<String> list1 = new LinkedList<>();
+
+        Map<String, String> map = new HashMap<>();
+
+    }
+}
